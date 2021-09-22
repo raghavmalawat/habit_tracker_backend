ef7138970c8fb0a7796d1221d7c66edbd775202a
Raghav Malawat <raghavmalawat@gmail.com>
2021-09-22 23:38:46 +0530

    Deleting daily habit logs

--- db/schema.rb
+++ db/schema.rb
@@ -17,21 +17,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_134311) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "daily_habits", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "habit_id", null: false
-    t.boolean "monday", default: false
-    t.boolean "tuesday", default: false
-    t.boolean "wednesday", default: false
-    t.boolean "thursday", default: false
-    t.boolean "friday", default: false
-    t.boolean "saturday", default: false
-    t.boolean "sunday", default: false
-    t.boolean "deleted", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["habit_id"], name: "index_daily_habits_on_habit_id"
-  end
-
   create_table "habit_logs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "user_id", null: false
     t.uuid "habit_id", null: false
@@ -67,7 +52,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_134311) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "daily_habits", "habits"
   add_foreign_key "habit_logs", "habits"
   add_foreign_key "habit_logs", "users"
   add_foreign_key "habits", "users"
